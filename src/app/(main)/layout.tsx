bda2877c7a711271d5b1d30e55491332862610c0
amit anand <dropmail.amit@gmail.com>
2025-07-10 18:09:08 +0000

    now shift the header to the top a bit more like remove the top margin an

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
     <div className="flex flex-col min-h-screen">
       <AnnouncementBanner />
       <Header />
-      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-4 pb-12">
         {children}
       </main>
       <Footer />
