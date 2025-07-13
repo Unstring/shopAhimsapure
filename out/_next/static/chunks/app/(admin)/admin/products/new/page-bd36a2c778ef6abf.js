07486e31dd09649eeeec9b63f8c3b9aa03e98810
amit anand <dropmail.amit@gmail.com>
2025-07-13 16:17:09 +0000

    auth and coupon

---
+++ out/_next/static/chunks/app/(admin)/admin/products/new/page-bd36a2c778ef6abf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3237],{12657:(e,s,_)=>{Promise.resolve().then(_.bind(_,26313))}},e=>{var s=s=>e(e.s=s);e.O(0,[4277,7417,7228,8441,1684,7358],()=>s(12657)),_N_E=e.O()}]);
