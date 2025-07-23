e0da41c338b18815bad20cc9ed3039aa21d4aafc
amit anand <dropmail.amit@gmail.com>
2025-07-23 06:25:54 +0000

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,12 @@ export default function RootLayout({
         <meta name="description" content="Ethically sourced, naturally grown, and delivered with love." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        {/* <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" /> */}
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         <AppProviders>
